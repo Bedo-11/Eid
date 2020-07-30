36216e4253eeb6cb857ea59e4932922070215f22
Bedo-11 <ibedo3@gmail.com>
2020-07-30 15:41:19 +0300

    checked style3

--- app.js
+++ app.js
@@ -97,48 +97,48 @@
   }
 
   // Some random colors
-const colors = ["#3CC157", "#2AA7FF", "#c90c0c", "#FCBC0F", "#F85F36"];
+// const colors = ["#3CC157", "#2AA7FF", "#c90c0c", "#FCBC0F", "#F85F36"];
 
-const numBalls = 50;
-const balls = [];
+// const numBalls = 50;
+// const balls = [];
 
-for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
-  ball.classList.add("ball");
-  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-  ball.style.transform = `scale(${Math.random()})`;
-  ball.style.width = `1em`;
-  ball.style.height = ball.style.width;
-  ball.style.zIndex = "-1";
+// for (let i = 0; i < numBalls; i++) {
+//   let ball = document.createElement("div");
+//   ball.classList.add("ball");
+//   ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+//   ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+//   ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+//   ball.style.transform = `scale(${Math.random()})`;
+//   ball.style.width = `1em`;
+//   ball.style.height = ball.style.width;
+//   ball.style.zIndex = "-1";
   
-  balls.push(ball);
-  document.body.append(ball);
-}
+//   balls.push(ball);
+//   document.body.append(ball);
+// }
 
 
-// Keyframes
-balls.forEach((el, i, ra) => {
-  let to = {
-    x: Math.random() * (i % 2 === 0 ? -11 : 11),
-    y: Math.random() * 12
-  };
+// // Keyframes
+// balls.forEach((el, i, ra) => {
+//   let to = {
+//     x: Math.random() * (i % 2 === 0 ? -11 : 11),
+//     y: Math.random() * 12
+//   };
 
-  let anim = el.animate(
-    [
-      { transform: "translate(0, 0)" },
-      { transform: `translate(${to.x}rem, ${to.y}rem)` }
-    ],
-    {
-      duration: (Math.random() + 1) * 2000, // random duration
-      direction: "alternate",
-      fill: "both",
-      iterations: Infinity,
-      easing: "ease-in-out"
-    }
-  );
-});
+//   let anim = el.animate(
+//     [
+//       { transform: "translate(0, 0)" },
+//       { transform: `translate(${to.x}rem, ${to.y}rem)` }
+//     ],
+//     {
+//       duration: (Math.random() + 1) * 2000, // random duration
+//       direction: "alternate",
+//       fill: "both",
+//       iterations: Infinity,
+//       easing: "ease-in-out"
+//     }
+//   );
+// });
 
 
  
